075d7f88baca705320fddfdea6d0e97257f7718e
Jilvan <jilvan.pereira@gold360.com.br>
2018-09-06 09:51:11 -0300

    Rodando no Firefox

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -3,18 +3,33 @@ require 'selenium-webdriver'
 require 'site_prism'
 require_relative 'page_helper.rb'
 
+BROWSER = ENV['BROWSER']
 AMBIENTE = ENV['AMBIENTE'] #homolog
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "\\ambientes\\#{AMBIENTE}.yml") #homolog.yml
 
 World(PageObjects)
 
+Capybara.register_driver :selenium do |app|
+
+if BROWSER.eql?('chrome')
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+elsif BROWSER.eql?('firefox')
+    Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true)
+elsif BROWSER.eql?('ie')
+    Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+elsif BROWSER.eql?('safari')
+    Capybara::Selenium::Driver.new(app, :browser => :safari)
+elsif BROWSER.eql?('Poltergeist')
+    options = { js_errors: false}
+    Capybara::Poltergeist::Driver.new(app, options)
+end
+
+end
+
 Capybara.configure do |config|
     #selenium selenium_chrome selenium_chrome_healess
-    config.default_driver = :selenium_chrome #selecionando navegador padrão
+    config.default_driver = :selenium #selecionando navegador padrão
     config.app_host = CONFIG['url_padrao'] #definindo site default para entrar
     config.default_max_wait_time = 5 #definindo tempo de 5 segundos para aguardar encontrar elemento
 end
-
-
-
