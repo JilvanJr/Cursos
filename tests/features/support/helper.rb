e1de2718f4e16c5e9329e2df8c01499dfb9e1f12
Jilvan <jilvan.pereira@gold360.com.br>
2018-09-11 14:09:36 -0300

    Modificando helper.rb

--- tests/features/support/helper.rb
+++ tests/features/support/helper.rb
@@ -1,10 +1,10 @@
 module Helper
 
     def tirar_foto(nome_arquivo, resultado)
-        caminho_arquivo = "C:\\Users\\jilvan.pereira\\Desktop\\Automacao\\siteprismteste\\results\\screenshots\\test_#{resultado}"
+        caminho_arquivo = "results\\screenshots\\test_#{resultado}"
         foto = "#{caminho_arquivo}\\#{nome_arquivo}.png"
         page.save_screenshot(foto)
-        embed(foto, 'image\\png','Clique aqui')
+        embed(foto, '..\\image\\png','Clique aqui')
     end
 end
 
